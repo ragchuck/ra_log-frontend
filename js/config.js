cc28eb2e8f11e6e93c8a76ad19eae9a9282132cd
Ragchuck <ragchuck@gmail.com>
2020-07-22 19:54:28 +0200

    normalize line endings

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 angular.module('ra_log.config', [])
 
     .constant('CONFIG', {
-    	COUCH_DB_URL: 'https://ralog:sunny@ralog.ragchuck.net/couchdb/',
+    	COUCH_DB_URL: 'https://ralog:sunny@'+location.hostname+'/couchdb/',
     	WEBBOX_KEY: 'WRTL1EBA:2100122532',
     	BEST_YEAR: "2011",
     	WORST_YEAR: "2013"
